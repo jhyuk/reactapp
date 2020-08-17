bd17867336257741fb3e9270cc1d9c0eb27a8417
jhyuk <akajhyuk@gmail.com>
2020-08-18 01:18:50 +0900

    기본 react app에 docker, docker-compose 세팅

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Test/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Test/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Test/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Test/i);
   expect(linkElement).toBeInTheDocument();
 });
